081f9db520444a071a6500db051d4b30b0efb9d7
TtPhong-FS <nam71441@gmail.com>
2026-04-05 02:25:12 +0000

    custom flow deloy

---
+++ scripts/vercel-build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+echo "Injecting npm auth..."
+
+echo "//npm.pkg.github.com/:_authToken=$NODE_AUTH_TOKEN" >> .npmrc
+
+echo "Installing deps..."
+pnpm install --frozen-lockfile
+
+echo "Building..."
+pnpm build
